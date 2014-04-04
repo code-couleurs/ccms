0dba1244179c11b9242bf8e018d4131087d6ba80
eric <e.marguin@agence-codecouleurs.fr>
2014-04-04 10:57:54 +0200

    doc install

--- fuel/app/bootstrap.php
+++ fuel/app/bootstrap.php
@@ -24,7 +24,7 @@ Autoloader::register();
  * Fuel::STAGE
  * Fuel::PRODUCTION
  */
-Fuel::$env = (isset($_SERVER['FUEL_ENV']) ? $_SERVER['FUEL_ENV'] : 'development');
+Fuel::$env = (isset($_SERVER['FUEL_ENV']) ? $_SERVER['FUEL_ENV'] : 'production');
 
 // Initialize the framework with the config file.
 Fuel::init('config.php');
